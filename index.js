43b87850a888a2372f3b73e526cb2d5a86d78d57
Jeevan Paudel <jeevanpaudel77@gmail.com>
2023-08-02 21:11:33 +0545

    added necessary changes

---
+++ index.js
@@ -0,0 +1,18 @@
+const playlist = [
+  "Get Inspired",
+  "Healthy Eating",
+  "Peace in the Pandemic",
+  "Good Vibrations",
+  "Healthy Mind",
+  "Relax & Recharge",
+];
+
+console.log(playlist[Math.floor(Math.random() * playlist.length)]);
+
+// / get random index value
+//     const randomIndex = Math.floor(Math.random() * arr.length);
+
+//     // get random item
+//     const item = arr[randomIndex];
+
+//     return item;
